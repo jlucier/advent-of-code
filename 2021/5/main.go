6816d57cb5324ed46e13e65c7bec15e49cbed757
Jordan Lucier <jordan@jordanlucier.com>
2021-12-25 19:12:21 -0500

    Indents

--- 2021/5/main.go
+++ 2021/5/main.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+  "fmt"
+  "strings"
 
-	"aoc/utils"
+  "aoc/utils"
 )
 
 // Data strcutures
 
 type Point struct {
-	x int
-	y int
+  x int
+  y int
 }
 
 type Line struct {
-	p1 Point
-	p2 Point
+  p1 Point
+  p2 Point
 }
 
 func (l *Line) IsDiagonal() bool {
-	return !(l.p1.x == l.p2.x || l.p1.y == l.p2.y)
+  return !(l.p1.x == l.p2.x || l.p1.y == l.p2.y)
 }
 
 func (l *Line) MaxX() int {
-	return utils.Max(l.p1.x, l.p2.x)
+  return utils.Max(l.p1.x, l.p2.x)
 }
 
 func (l *Line) MinX() int {
-	return utils.Min(l.p1.x, l.p2.x)
+  return utils.Min(l.p1.x, l.p2.x)
 }
 
 func (l *Line) MaxY() int {
-	return utils.Max(l.p1.y, l.p2.y)
+  return utils.Max(l.p1.y, l.p2.y)
 }
 
 func (l *Line) MinY() int {
-	return utils.Min(l.p1.y, l.p2.y)
+  return utils.Min(l.p1.y, l.p2.y)
 }
 
 func (l *Line) Coords() [][2]int {
-	var coords [][2]int
-	x := l.p1.x
-	y := l.p1.y
+  var coords [][2]int
+  x := l.p1.x
+  y := l.p1.y
   dx := 1
   dy := 1
   if l.p2.x < l.p1.x {
@@ -52,88 +52,88 @@ func (l *Line) Coords() [][2]int {
     dy = -1
   }
 
-	n := utils.Max(l.MaxY()-l.MinY(), l.MaxX()-l.MinX())
+  n := utils.Max(l.MaxY()-l.MinY(), l.MaxX()-l.MinX())
 
-	diag := l.IsDiagonal()
-	for i := 0; i <= n; i++ {
-		coords = append(coords, [2]int{y, x})
+  diag := l.IsDiagonal()
+  for i := 0; i <= n; i++ {
+    coords = append(coords, [2]int{y, x})
     if diag || l.p1.x == l.p2.x {
       y += dy
     }
     if diag || l.p1.y == l.p2.y {
       x += dx
     }
-	}
+  }
 
-	return coords
+  return coords
 }
 
 // helpers
 
 func parseLine(s string) Line {
-	tokens := strings.Split(s, " -> ")
-	if len(tokens) != 2 {
-		panic("WTF")
-	}
+  tokens := strings.Split(s, " -> ")
+  if len(tokens) != 2 {
+    panic("WTF")
+  }
 
-	p1 := utils.StrsToInts(strings.Split(tokens[0], ","))
-	p2 := utils.StrsToInts(strings.Split(tokens[1], ","))
-	return Line{Point{p1[0], p1[1]}, Point{p2[0], p2[1]}}
+  p1 := utils.StrsToInts(strings.Split(tokens[0], ","))
+  p2 := utils.StrsToInts(strings.Split(tokens[1], ","))
+  return Line{Point{p1[0], p1[1]}, Point{p2[0], p2[1]}}
 }
 
 func makeBoard(ls []string, diag bool) ([][]int, []Line) {
-	var lines []Line
+  var lines []Line
 
-	maxX := 0
-	maxY := 0
-	for _, l := range ls {
-		line := parseLine(l)
-		if !diag && line.IsDiagonal() {
-			continue
-		}
-		maxX = utils.Max(maxX, line.MaxX())
-		maxY = utils.Max(maxY, line.MaxY())
-		lines = append(lines, line)
-	}
+  maxX := 0
+  maxY := 0
+  for _, l := range ls {
+    line := parseLine(l)
+    if !diag && line.IsDiagonal() {
+      continue
+    }
+    maxX = utils.Max(maxX, line.MaxX())
+    maxY = utils.Max(maxY, line.MaxY())
+    lines = append(lines, line)
+  }
 
-	var board [][]int
-	for i := 0; i <= maxY; i++ {
-		board = append(board, make([]int, maxX+1))
-	}
+  var board [][]int
+  for i := 0; i <= maxY; i++ {
+    board = append(board, make([]int, maxX+1))
+  }
 
-	for _, l := range lines {
-		for _, c := range l.Coords() {
-			board[c[0]][c[1]] += 1
-		}
-	}
-	return board, lines
+  for _, l := range lines {
+    for _, c := range l.Coords() {
+      board[c[0]][c[1]] += 1
+    }
+  }
+  return board, lines
 }
 
 func solve(diag bool) {
-	ls := utils.ReadLines("inp.txt")
-	board, _ := makeBoard(ls, diag)
-	count := 0
-	for _, r := range board {
-		for _, v := range r {
-			if v >= 2 {
-				count += 1
-			}
-		}
-	}
-	// for _, r := range board {
-	// 	fmt.Println(r)
-	// }
-	fmt.Println("count:", count)
+  ls := utils.ReadLines("inp.txt")
+  board, _ := makeBoard(ls, diag)
+  count := 0
+  for _, r := range board {
+    for _, v := range r {
+      if v >= 2 {
+        count += 1
+      }
+    }
+  }
+  // for _, r := range board {
+  //  fmt.Println(r)
+  // }
+  fmt.Println("count:", count)
 }
 
 func part1() {
-	solve(false)
+  solve(false)
 }
 
 func part2() {
-	solve(true)
+  solve(true)
 }
 
 func main() {
-	part2()
+  part2()
 }
