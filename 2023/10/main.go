17716e9ad23e425df4544f2703c6135245d480ce
jlucier <jordan@jordanlucier.com>
2023-12-10 21:54:29 -0500

    gottem

--- 2023/10/main.go
+++ 2023/10/main.go
@@ -20,26 +20,6 @@ type Cell struct {
 	col int
 }
 
-func (self *Cell) Add(other *Cell) Cell {
-	return Cell{self.row + other.row, self.col + other.col}
-}
-
-func (self *Cell) Rotate(angle int) Cell {
-	switch angle {
-	case 90:
-		return Cell{-self.col, self.row}
-	case -90:
-		return Cell{self.col, -self.row}
-	case 0:
-		return *self
-	}
-	panic("Unhandled rotation")
-}
-
-func (self *Cell) Diff(other *Cell) Cell {
-	return Cell{self.row - other.row, self.col - other.col}
-}
-
 func (self *Cell) Up() Cell {
 	return Cell{self.row - 1, self.col}
 }
@@ -194,187 +174,79 @@ func (self *Map) Walk() []Cell {
 	}
 }
 
-func (self *Map) findSomethingExterior(path []Cell) Cell {
+func (self *Map) countSAs(path []Cell) byte {
+	start := path[0]
+	startNeighbors := utils.NewSet(self.ConnectedNeighbors(&start))
+
+	if startNeighbors.Contains(start.Up()) {
+		if startNeighbors.Contains(start.Down()) {
+			return '|'
+		} else if startNeighbors.Contains(start.Right()) {
+			return 'L'
+		} else if startNeighbors.Contains(start.Left()) {
+			return 'J'
+		}
+	} else if startNeighbors.Contains(start.Down()) {
+		if startNeighbors.Contains(start.Right()) {
+			return 'F'
+		} else if startNeighbors.Contains(start.Left()) {
+			return '7'
+		}
+	}
+	return '-'
+}
+
+func (self *Map) NumContained(path []Cell) int {
+	sCounts := self.countSAs(path)
+	start := path[0]
+	self.grid[start.row][start.col] = sCounts
+
 	pathCells := utils.NewSet(path)
+	tot := 0
+	insideCells := utils.EmptySet[Cell]()
+	upCorners := "LJ"
+	downCorners := "F7"
 
 	for i := 0; i < len(self.grid); i++ {
-		le := Cell{i, 0}
-		re := Cell{i, len(self.grid[0]) - 1}
+		inside := false
+		seenCorners := 0
+		for j := 0; j < len(self.grid[0]); j++ {
+			b := self.grid[i][j]
 
-		if !pathCells.Contains(le) {
-			return le
-		} else if !pathCells.Contains(re) {
-			return re
-		}
-	}
-
-	for i := 0; i < len(self.grid[0]); i++ {
-		te := Cell{0, i}
-		be := Cell{len(self.grid) - 1, i}
-
-		if !pathCells.Contains(te) {
-			return te
-		} else if !pathCells.Contains(be) {
-			return be
-		}
-	}
-	return Cell{}
-}
-
-func (self *Map) findAllAdjacent(cell Cell, seen *utils.Set[Cell]) {
-	opts := []Cell{
-		cell.Up(),
-		cell.Down(),
-		cell.Left(),
-		cell.Right(),
-	}
-
-	for _, o := range opts {
-		if !seen.Contains(o) &&
-			utils.Between(o.row, 0, len(self.grid)) && utils.Between(o.col, 0, len(self.grid[0])) {
-			seen.Add(o)
-			self.findAllAdjacent(o, seen)
-		}
-	}
-}
-
-func (self *Map) WalkNextTo(path []Cell) {
-	pathCells := utils.NewSet(path)
-
-	ext := self.findSomethingExterior(path)
-	grp := utils.NewSet(path)
-	grp.Add(ext)
-	self.findAllAdjacent(ext, &grp)
-	grp.RemoveAll(path)
-
-	var extC Cell
-	var pathC Cell
-
-	for _, opt := range grp.Values() {
-		pathNb := pathCells.Intersection([]Cell{opt.Up(), opt.Down(), opt.Left(), opt.Right()})
-		// fmt.Println(opt, pathNb)
-		if pathNb.Size() > 0 {
-			extC = opt
-			pathC = pathNb.Values()[0]
-			break
-		}
-	}
-
-	pathI := 0
-	for i, c := range path {
-		if c == pathC {
-			pathI = i
-			break
-		}
-	}
-
-	extOrPath := pathCells.Copy()
-	for i := 0; i < len(path); i++ {
-		if !pathCells.Contains(extC) {
-			// find any unkown
-			self.findAllAdjacent(extC, &extOrPath)
-		}
-
-		// move extC accordingly
-		ri := (pathI + i) % len(path)
-		// fmt.Println(pathI, i, len(path), ri)
-		// if ri+1 >= pathI {
-		// 	break
-		// }
-
-		curr := path[ri]
-		next := path[(ri+1)%len(path)]
-		nextMove := next.Diff(&curr)
-		// TODO we have issues with repeated tight bends making our
-		// offset dude crazy
-
-		delta := extC.Diff(&curr)
-
-		fmt.Println("path", curr, "ext", extC, "d", delta)
-		self.Print([]Cell{curr, extC}, []Cell{})
-
-		if strings.ContainsRune(CORNERS, rune(self.Get(&curr))) {
-			// corner
-			tmpD := delta.Rotate(getTurn(&delta, self.Get(&curr)))
-			nextExt := next.Add(&tmpD)
-			// fmt.Println("After", nextExt)
-
-			missed := curr.Add(&tmpD)
-			// need to check neighbors?
-			if !pathCells.Contains(missed) {
-				fmt.Println("checking missed", missed)
-				self.Print([]Cell{missed}, []Cell{})
-				self.findAllAdjacent(missed, &extOrPath)
+			if pathCells.Contains(Cell{i, j}) {
+				// hitting path
+				if b == '|' {
+					inside = !inside
+					seenCorners = 0
+				} else if strings.ContainsRune(upCorners, rune(b)) {
+					seenCorners++
+					if seenCorners == 0 {
+						inside = !inside
+					} else if seenCorners > 1 {
+						seenCorners = 0
+					}
+				} else if strings.ContainsRune(downCorners, rune(b)) {
+					seenCorners--
+					if seenCorners == 0 {
+						inside = !inside
+					} else if seenCorners < -1 {
+						seenCorners = 0
+					}
+				}
+			} else if inside {
+				insideCells.Add(Cell{i, j})
+				tot++
 			}
-			extC = nextExt
-		} else {
-			extC = extC.Add(&nextMove)
 		}
-
-		// fmt.Println(nextMove, delta)
-		// self.Print([]Cell{curr, extC}, []Cell{})
-		//
-		// if strings.ContainsRune(CORNERS, rune(self.Get(&curr))) &&
-		// 	((nextMove.row != 0 && delta.row != 0) || (nextMove.col != 0 && delta.col != 0)) {
-		// 	adj := utils.NewSet([]Cell{
-		// 		curr.Up(),
-		// 		curr.Down(),
-		// 		curr.Left(),
-		// 		curr.Right(),
-		// 	})
-		// 	adj.Remove(extC)
-		// 	adj.Remove(next)
-		// 	prev := path[(ri-1+len(path))%len(path)]
-		// 	adj.Remove(prev)
-		// 	fmt.Println(extC, adj.Values()[0])
-		//
-		// 	if adj.Size() != 1 {
-		// 		fmt.Println(curr, adj.Values())
-		// 		panic("FUCK")
-		// 	}
-		// 	// move ext dude to the only remaining neighbor before we add the delta
-		// 	extC = adj.Values()[0]
-		// 	// also find here since we'll skip it
-		// 	self.findAllAdjacent(extC, &extOrPath)
-		// }
-		//
-		// extC = extC.Add(&nextMove)
 	}
 
-	self.Print([]Cell{}, extOrPath.Values())
-
-	fmt.Println("p2:", len(self.grid)*len(self.grid[0])-extOrPath.Size())
-}
-
-func getTurn(d *Cell, letter byte) int {
-	switch letter {
-	case 'L':
-		if d.row != 0 {
-			return -90
-		}
-		return 90
-	case 'J':
-		if d.row != 0 {
-			return 90
-		}
-		return -90
-	case 'F':
-		if d.row != 0 {
-			return 90
-		}
-		return -90
-	case '7':
-		if d.row != 0 {
-			return -90
-		}
-		return 90
-	}
-	return 0
+	self.Print(path, insideCells.Values())
+	return tot
 }
 
 func main() {
-	m := parseMap("~/sync/dev/aoc_inputs/2023/10/ex.txt")
+	m := parseMap("~/sync/dev/aoc_inputs/2023/10/input.txt")
 	p := m.Walk()
 	fmt.Println("p1:", len(p)/2)
-	m.WalkNextTo(p)
+	fmt.Println("p2:", m.NumContained(p))
 }
