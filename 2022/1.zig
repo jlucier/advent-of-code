9342935568a3103654850cf043f04aae03d65fbf
jlucier <jordan@jordanlucier.com>
2024-11-24 23:37:55 -0500

    finish 1

--- 2022/1.zig
+++ 2022/1.zig
@@ -3,10 +3,32 @@ const zutils = @import("zutils.zig");
 
 pub fn main() void {
     const allocator = std.heap.page_allocator;
-    const res1 = zutils.expandHomeDir(allocator, "~") catch {
-        std.debug.print("Bad alloc\n", .{});
+    const ll = zutils.readLines(allocator, "~/sync/dev/aoc_inputs/2022/1.txt") catch {
+        std.debug.print("Failed to read", .{});
         return;
     };
-    defer allocator.free(res1);
-    std.debug.print("{s}\n", .{res1});
+    defer ll.deinit();
+
+    var curr: u32 = 0;
+    var totals = std.ArrayList(u32).init(allocator);
+
+    for (ll.lines.items) |ln| {
+        if (ln.len == 0) {
+            totals.append(curr) catch {
+                std.debug.print("Failed to grow array\n", .{});
+                return;
+            };
+            curr = 0;
+        } else {
+            curr += std.fmt.parseInt(u32, ln, 10) catch {
+                std.debug.print("Failed to parse int: {s}", .{ln});
+                return;
+            };
+        }
+    }
+
+    std.mem.sort(u32, totals.items, {}, std.sort.desc(u32));
+
+    std.debug.print("p1: {d}\n", .{totals.items[0]});
+    std.debug.print("p2: {d}\n", .{zutils.sum(u32, totals.items[0..3])});
 }
--- 2022/zutils.zig
+++ 2022/zutils.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+// FS
+
 /// Expand the ~ in a pathname to the users home dir. Caller owns the returned path string
 pub fn expandHomeDir(allocator: std.mem.Allocator, pathname: []const u8) ![]u8 {
     if (pathname[0] == '~' and (pathname.len == 1 or pathname[1] == '/')) {
@@ -11,8 +13,24 @@ pub fn expandHomeDir(allocator: std.mem.Allocator, pathname: []const u8) ![]u8 {
     return allocator.dupe(u8, pathname);
 }
 
+const LineList = struct {
+    lines: std.ArrayList([]u8),
+    allocator: std.mem.Allocator,
+
+    pub fn init(alloc: std.mem.Allocator) LineList {
+        return LineList{ .lines = std.ArrayList([]u8).init(alloc), .allocator = alloc };
+    }
+
+    pub fn deinit(self: *const LineList) void {
+        for (self.lines.items) |ln| {
+            self.allocator.free(ln);
+        }
+        self.lines.deinit();
+    }
+};
+
 /// Read lines of a file. ArrayList and strings inside are owned by caller
-pub fn readFile(allocator: std.mem.Allocator, pathname: []const u8) !std.ArrayList([]u8) {
+pub fn readLines(allocator: std.mem.Allocator, pathname: []const u8) !LineList {
     const path = try expandHomeDir(allocator, pathname);
     defer allocator.free(path);
 
@@ -20,23 +38,38 @@ pub fn readFile(allocator: std.mem.Allocator, pathname: []const u8) !std.ArrayLi
     defer file.close();
     const reader = file.reader();
 
-    var lines = std.ArrayList([]u8).init(allocator);
+    var ll = LineList.init(allocator);
     while (true) {
         const ln = try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 1_000_000);
 
-        if (ln != null) {
-            try lines.append(ln.?);
+        if (ln) |l| {
+            try ll.lines.append(l);
         } else {
             break;
         }
     }
 
-    return lines;
+    return ll;
+}
+
+// Slices
+
+pub fn sum(comptime T: type, slice: []const T) T {
+    var s: T = 0;
+    for (slice) |el| s += el;
+    return s;
 }
 
 // TESTS
 
 test "expand home" {
+    const home = std.posix.getenv("HOME") orelse "";
+    const p1 = "/something/other";
+
+    const parts = [_][]const u8{ home, p1 };
+    const a2 = try std.fs.path.join(std.testing.allocator, &parts);
+    defer std.testing.allocator.free(a2);
+
     const res1 = try expandHomeDir(std.testing.allocator, "~");
     defer std.testing.allocator.free(res1);
 
@@ -46,26 +79,29 @@ test "expand home" {
     const res3 = try expandHomeDir(std.testing.allocator, "/something/other");
     defer std.testing.allocator.free(res3);
 
-    try std.testing.expect(std.mem.eql(u8, res1, "/home/jordan"));
-    try std.testing.expect(std.mem.eql(u8, res2, "/home/jordan/something/other"));
-    try std.testing.expect(std.mem.eql(u8, res3, "/something/other"));
+    try std.testing.expect(std.mem.eql(u8, res1, home));
+    try std.testing.expect(std.mem.eql(u8, res2, a2));
+    try std.testing.expect(std.mem.eql(u8, res3, p1));
 }
 
 test "read lines" {
     const f = try std.fs.cwd().realpathAlloc(std.testing.allocator, "test.txt");
     defer std.testing.allocator.free(f);
 
-    const lines = try readFile(std.testing.allocator, f);
-    defer {
-        for (lines.items) |ln| {
-            std.testing.allocator.free(ln);
-        }
-        lines.deinit();
-    }
+    const ll = try readLines(std.testing.allocator, f);
+    defer ll.deinit();
 
-    try std.testing.expect(std.mem.eql(u8, lines.items[0], "1000"));
-    try std.testing.expect(std.mem.eql(u8, lines.items[1], "2000"));
-    try std.testing.expect(std.mem.eql(u8, lines.items[2], "3000"));
-    try std.testing.expect(std.mem.eql(u8, lines.items[3], ""));
-    try std.testing.expect(std.mem.eql(u8, lines.items[4], "4000"));
+    try std.testing.expect(std.mem.eql(u8, ll.lines.items[0], "1000"));
+    try std.testing.expect(std.mem.eql(u8, ll.lines.items[1], "2000"));
+    try std.testing.expect(std.mem.eql(u8, ll.lines.items[2], "3000"));
+    try std.testing.expect(std.mem.eql(u8, ll.lines.items[3], ""));
+    try std.testing.expect(std.mem.eql(u8, ll.lines.items[4], "4000"));
+}
+
+test "sum" {
+    const a = [_]u8{ 1, 2, 3, 4 };
+    try std.testing.expectEqual(sum(u8, &a), 10);
+
+    const b = [_]i32{ -1, 2, 3, -4 };
+    try std.testing.expectEqual(sum(i32, &b), 0);
 }
