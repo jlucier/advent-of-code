70f10735224eff0cb1ab655f31b84c27c18f9bd1
jlucier <jordan@jordanlucier.com>
2024-12-07 15:54:21 -0500

    2024/7 Smart soln, very cool

--- 2024/7.zig
+++ 2024/7.zig
@@ -1,52 +1,6 @@
 const std = @import("std");
 const zutils = @import("zutils");
 
-const Operator = enum { ADD, MUL, CONCAT };
-
-const OpPerms = struct {
-    allocator: std.mem.Allocator,
-    perms: [][]Operator,
-
-    pub fn init(allocator: std.mem.Allocator, num_ops: usize, num_op_types: usize) !OpPerms {
-        const num_perms = std.math.pow(usize, num_op_types, num_ops);
-        const opms = OpPerms{
-            .allocator = allocator,
-            .perms = try allocator.alloc([]Operator, num_perms),
-        };
-
-        var i: usize = 0;
-        while (i < num_perms) : (i += 1) {
-            opms.perms[i] = try allocator.alloc(Operator, num_ops);
-        }
-
-        i = 0;
-        while (i < num_ops) : (i += 1) {
-            // for every slot, place each option for operator into that slot of every
-            // permutation, alternating every so often according to the slot index
-            var curr_op = Operator.ADD;
-            for (opms.perms, 0..) |p, j| {
-                p[i] = curr_op;
-                if ((j + 1) % std.math.pow(usize, num_op_types, i) == 0) {
-                    curr_op = switch (curr_op) {
-                        .ADD => .MUL,
-                        .MUL => if (num_op_types == 2) .ADD else .CONCAT,
-                        .CONCAT => .ADD,
-                    };
-                }
-            }
-        }
-
-        return opms;
-    }
-
-    pub fn deinit(self: *const OpPerms) void {
-        for (self.perms) |p| {
-            self.allocator.free(p);
-        }
-        self.allocator.free(self.perms);
-    }
-};
-
 fn parseLine(allocator: std.mem.Allocator, ln: []const u8) ![]usize {
     var sl = try allocator.alloc(usize, std.mem.count(u8, ln, " ") + 1);
     var iter = std.mem.splitScalar(u8, ln, ' ');
@@ -61,33 +15,46 @@ fn parseLine(allocator: std.mem.Allocator, ln: []const u8) ![]usize {
     return sl;
 }
 
-fn canSolve(allocator: std.mem.Allocator, eq: []usize, nops: usize) !?usize {
-    const test_val = eq[0];
-    // len - 1 = num components, num components - 1 = num ops
-    const opms = try OpPerms.init(allocator, eq.len - 2, nops);
-    defer opms.deinit();
+fn solveBackwards(allocator: std.mem.Allocator, eq: []const usize, nops: usize) !?usize {
+    var stack = try std.ArrayList([2]usize).initCapacity(allocator, 1);
+    defer stack.deinit();
+    stack.appendAssumeCapacity(.{ eq[0], eq.len - 1 });
 
-    // for (opms.perms) |ops| {
-    //     std.debug.print("{any}\n", .{ops});
-    // }
+    while (stack.popOrNull()) |node| {
+        const curr = node[0];
+        const i = node[1];
+        const operand = eq[i];
+        // std.debug.print("eval: {any} operand: {d}\n", .{ node, operand });
 
-    for (opms.perms) |ops| {
-        var curr: usize = eq[1];
-        for (eq[2..], 0..) |v, i| {
-            // std.debug.print("curr: {d} v: {d} op: {}\n", .{ curr, v, ops[i] });
-            curr = switch (ops[i]) {
-                .ADD => curr + v,
-                .MUL => curr * v,
-                .CONCAT => curr * std.math.pow(usize, 10, std.math.log10_int(v) + 1) + v,
-            };
-
-            if (curr > test_val) {
-                // there is no op that reduces the number
-                break;
+        if (i == 1) {
+            // we're at the end, don't go further
+            if (curr == operand) {
+                // solved
+                // std.debug.print("solved\n", .{});
+                return eq[0];
             }
-        }
-        if (curr == test_val) {
-            return test_val;
+            // not sovled, nothing to do
+        } else {
+            // explore branches, subject to rules
+
+            // add
+            if (curr > operand) {
+                try stack.append(.{ curr - operand, i - 1 });
+            }
+
+            // mul (only if divisible)
+            if (curr % operand == 0) {
+                try stack.append(.{ curr / operand, i - 1 });
+            }
+
+            if (nops == 3) {
+                // concat (only if ends with)
+                if (curr > operand and (curr - operand) % 10 == 0) {
+                    const factor = std.math.pow(usize, 10, std.math.log10_int(operand) + 1);
+                    // std.debug.print("    adding: {d} {d}\n", .{ curr / factor, factor });
+                    try stack.append(.{ curr / factor, i - 1 });
+                }
+            }
         }
     }
     return null;
@@ -101,15 +68,13 @@ fn checkSolvable(allocator: std.mem.Allocator, lines: []const []const u8) ![2]us
         const eq = try parseLine(allocator, ln);
         defer allocator.free(eq);
 
-        if (try canSolve(allocator, eq, 2)) |v| {
+        if (try solveBackwards(allocator, eq, 2)) |v| {
             p1 += v;
-        }
-
-        if (try canSolve(allocator, eq, 3)) |v| {
+        } else if (try solveBackwards(allocator, eq, 3)) |v| {
             p2 += v;
         }
     }
-    return .{ p1, p2 };
+    return .{ p1, p1 + p2 };
 }
 
 test "part" {
@@ -131,10 +96,95 @@ test "part" {
 }
 
 pub fn main() !void {
-    const lines = try zutils.readLines(std.heap.page_allocator, "~/sync/dev/aoc_inputs/2024/7.txt");
-    defer lines.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
-    const res = try checkSolvable(std.heap.page_allocator, lines.strings.items);
+    const lines = try zutils.readLines(allocator, "~/sync/dev/aoc_inputs/2024/7.txt");
+
+    const res = try checkSolvable(allocator, lines.strings.items);
     std.debug.print("p1: {d}\n", .{res[0]});
     std.debug.print("p2: {d}\n", .{res[1]});
 }
+
+// NOTE: this code did work, it was my "small brain" solution. I liked parts of it
+// and didn't want to just throw it away. Particularly, the initialization of all the
+// permutations of operations was fun. I had never thought about that small problem before.
+// This solution had basically nothing in the way of branch pruning, which was an obvious sign
+// I was "doing it wrong", but it did actually finish running in a little less than 2 minutes
+// on my laptop. Yay zig I guess. The new solution however, is very cool and runs both parts in
+// 50ms. Slightly disappointed I didn't see it myself, but happy to have tried it out.
+//
+//
+// const Operator = enum { ADD, MUL, CONCAT };
+//
+// const OpPerms = struct {
+//     allocator: std.mem.Allocator,
+//     perms: [][]Operator,
+//
+//     pub fn init(allocator: std.mem.Allocator, num_ops: usize, num_op_types: usize) !OpPerms {
+//         const num_perms = std.math.pow(usize, num_op_types, num_ops);
+//         const opms = OpPerms{
+//             .allocator = allocator,
+//             .perms = try allocator.alloc([]Operator, num_perms),
+//         };
+//
+//         var i: usize = 0;
+//         while (i < num_perms) : (i += 1) {
+//             opms.perms[i] = try allocator.alloc(Operator, num_ops);
+//         }
+//
+//         i = 0;
+//         while (i < num_ops) : (i += 1) {
+//             // for every slot, place each option for operator into that slot of every
+//             // permutation, alternating every so often according to the slot index
+//             var curr_op = Operator.ADD;
+//             for (opms.perms, 0..) |p, j| {
+//                 p[i] = curr_op;
+//                 if ((j + 1) % std.math.pow(usize, num_op_types, i) == 0) {
+//                     curr_op = switch (curr_op) {
+//                         .ADD => .MUL,
+//                         .MUL => if (num_op_types == 2) .ADD else .CONCAT,
+//                         .CONCAT => .ADD,
+//                     };
+//                 }
+//             }
+//         }
+//
+//         return opms;
+//     }
+//
+//     pub fn deinit(self: *const OpPerms) void {
+//         for (self.perms) |p| {
+//             self.allocator.free(p);
+//         }
+//         self.allocator.free(self.perms);
+//     }
+// };
+//
+// fn canSolve(allocator: std.mem.Allocator, eq: []const usize, nops: usize) !?usize {
+//     const test_val = eq[0];
+//     // len - 1 = num components, num components - 1 = num ops
+//     const opms = try OpPerms.init(allocator, eq.len - 2, nops);
+//     defer opms.deinit();
+//
+//     for (opms.perms) |ops| {
+//         var curr: usize = eq[1];
+//         for (eq[2..], 0..) |v, i| {
+//             curr = switch (ops[i]) {
+//                 .ADD => curr + v,
+//                 .MUL => curr * v,
+//                 .CONCAT => curr * std.math.pow(usize, 10, std.math.log10_int(v) + 1) + v,
+//             };
+//
+//             if (curr > test_val) {
+//                 // there is no op that reduces the number
+//                 break;
+//             }
+//         }
+//         if (curr == test_val) {
+//             return test_val;
+//         }
+//     }
+//     return null;
+// }
