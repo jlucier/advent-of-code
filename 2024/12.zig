cbb4327d7713376ef066c669b236f287c0758729
jlucier <jordan@jordanlucier.com>
2024-12-15 23:21:50 -0500

    Stash 2024/12

--- 2024/12.zig
+++ 2024/12.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const zutils = @import("zutils");
 
 const V2 = zutils.V2(isize);
+const Lines = []const []const u8;
 
 const Region = struct {
     crop: u8,
@@ -23,7 +24,7 @@ const Region = struct {
     }
 };
 
-fn printRegion(lines: []const []const u8, region: *const Region) void {
+fn printRegion(lines: Lines, region: *const Region) void {
     for (lines, 0..) |ln, j| {
         for (ln, 0..) |c, i| {
             if (region.border.get(.{ .x = @intCast(i), .y = @intCast(j) }) != null) {
@@ -37,21 +38,7 @@ fn printRegion(lines: []const []const u8, region: *const Region) void {
     std.debug.print("\n", .{});
 }
 
-fn printPoint(lines: []const []const u8, v: V2) void {
-    for (lines, 0..) |ln, j| {
-        for (ln, 0..) |c, i| {
-            if (v.equal(.{ .x = @intCast(i), .y = @intCast(j) })) {
-                zutils.printRed(ln[i .. i + 1]);
-            } else {
-                std.debug.print("{c}", .{c});
-            }
-        }
-        std.debug.print("\n", .{});
-    }
-    std.debug.print("\n", .{});
-}
-
-fn parseRegions(allocator: std.mem.Allocator, lines: []const []const u8) !std.ArrayList(Region) {
+fn parseRegions(allocator: std.mem.Allocator, lines: Lines) !std.ArrayList(Region) {
     var regions = std.ArrayList(Region).init(allocator);
     var seen = std.AutoHashMap(V2, void).init(allocator);
     defer seen.deinit();
@@ -78,7 +65,7 @@ fn expandRegion(
     seen: *std.AutoHashMap(V2, void),
     region: *Region,
     start: V2,
-    lines: []const []const u8,
+    lines: Lines,
 ) !void {
     var queue = try std.ArrayList(V2).initCapacity(allocator, 1);
     defer queue.deinit();
@@ -106,101 +93,179 @@ fn expandRegion(
     }
 }
 
-fn inBounds(lines: []const []const u8, v: V2) bool {
+fn inBounds(lines: Lines, v: V2) bool {
     return v.inGridBounds(@intCast(lines[0].len), @intCast(lines.len));
 }
 
-fn getCrop(lines: []const []const u8, v: V2) ?u8 {
+fn getCrop(lines: Lines, v: V2) ?u8 {
     if (inBounds(lines, v))
         return lines[@intCast(v.y)][@intCast(v.x)];
     return null;
 }
 
-fn colIndexOf(lines: []const []const u8, col: usize, scalar: u8) ?usize {
-    var i: usize = 0;
-    while (i < lines.len) : (i += 1) {
-        if (lines[i][col] == scalar) {
-            return i;
-        }
-    }
-    return null;
-}
-
-fn lastColIndexOf(lines: []const []const u8, col: usize, scalar: u8) ?usize {
-    var i: isize = @intCast(lines.len - 1);
-    while (i >= 0) : (i -= 1) {
-        if (lines[@intCast(i)][col] == scalar) {
-            return @intCast(i);
-        }
-    }
-    return null;
-}
-
-fn findRegionSides(allocator: std.mem.Allocator, region: *const Region, lines: []const []const u8) !usize {
-    var to_process = try region.border.clone();
-    defer to_process.deinit();
-
-    var sides: usize = 0;
-    var dir = V2{ .x = -1 };
-    while (to_process.popOrNull()) |p| {
-        const v = p.key;
+/// Find a neighbors of the border of the region
+fn getOutline(
+    allocator: std.mem.Allocator,
+    region: *const Region,
+    lines: Lines,
+) !std.AutoArrayHashMap(V2, void) {
+    var ret = std.AutoArrayHashMap(V2, void).init(allocator);
+    for (region.border.keys()) |v| {
         for (v.neighbors()) |n| {
-            if (getCrop(lines, n) != region.crop) {}
+            // includes nulls
+            if (getCrop(lines, n) != region.crop) {
+                try ret.put(n, {});
+            }
+        }
+    }
+    return ret;
+}
+
+fn borderPathExists(lines: Lines, region: *const Region, start: V2, end: V2) bool {
+    var v = start;
+    const dir = end.sub(start).unit(f32).asType(isize);
+    // std.debug.print("    hmm: {} {} {}\n", .{ start, dir, end });
+    while (!v.equal(end)) {
+        if (getCrop(lines, v) != region.crop or region.border.get(v) == null) {
+            return false;
+        }
+        v.addMut(dir);
+    }
+    return true;
+}
+
+fn findDiffNeighbor(lines: Lines, v: V2, crop: u8) ?V2 {
+    for (v.neighbors()) |n| {
+        if (getCrop(lines, n) != crop) {
+            return n;
+        }
+    }
+    return null;
+}
+
+fn findRegionSides(allocator: std.mem.Allocator, region: *const Region, lines: Lines) !usize {
+    var sides: usize = 0;
+
+    const start_wall = region.border.keys()[0];
+    const start = findDiffNeighbor(lines, start_wall, region.crop).?;
+    std.debug.print("start_wall: {} start: {}\n", .{ start_wall, start });
+
+    var curr = start;
+    var wall_dir = start_wall.sub(start);
+    var move_dir = wall_dir.rotateCounterClockwise();
+    std.debug.print("move: {} wall: {}\n", .{ move_dir, wall_dir });
+
+    const grid = try zutils.Grid(u8).init2DSlice(allocator, lines);
+    defer grid.deinit();
+    var hl = [2]zutils.V2(usize){ start.asType(usize), curr.asType(usize) };
+
+    var internal = false;
+    while (true) {
+        std.debug.print("hmm: {}\n", .{curr});
+        if (curr.x >= 0 and curr.y >= 0) {
+            hl[1] = curr.asType(usize);
+            grid.printHl(&hl);
+        } else {
+            const tmp = [_]zutils.V2(usize){start.asType(usize)};
+            grid.printHl(&tmp);
+        }
+        var next = curr.add(move_dir);
+        if (next.equal(start)) {
+            return sides;
         }
 
-        printPoint(lines, p);
-        sides += 1;
+        var count = true;
+        if (getCrop(lines, next) == region.crop) {
+            std.debug.print("internal\n", .{});
+            // internal
+            sides += 1;
+            wall_dir = wall_dir.rotateCounterClockwise();
+            move_dir = move_dir.rotateCounterClockwise();
+            internal = true;
+            count = false;
+        } else if (getCrop(lines, next.add(wall_dir)) != region.crop) {
+            // ran past
+            if (internal) {
+                // already hit internal, diag
+                next.addMut(wall_dir);
+                if (next.equal(start)) {
+                    return sides;
+                }
+            } else {
+                std.debug.print("past\n", .{});
+                sides += 1;
+                wall_dir = wall_dir.rotateClockwise();
+                move_dir = move_dir.rotateClockwise();
+                count = false;
+            }
+        }
+
+        if (count) {
+            curr = next;
+            internal = false;
+        }
     }
 
     return sides;
 
-    // for (region.border.keys()) |v| {
-    // for each neighbor which is not a region member check the caches
-    // use row cache when normal is vertical, col cache if horizontal
+    // OLD
 
-    // for (v.neighbors()) |n| {
-    //     if (!n.inGridBounds(lines.len, lines[0].len) or
-    //         lines[@intCast(n.y)][@intCast(n.x)] == region.crop)
-    //     {
-    //         continue;
-    //     }
-    // }
-    // }
+    // 1. pick cell
+    // 2. for each dir that is edge
+    //   a. for other borders in the same col if looking horiz, row if vert
+    //   b. if a path exists between original and that point, same side
+    //   c. cache this direction for everything same side
 
+    // cache indices corresponde to each neighbor sequentially
+    // var cache = std.AutoHashMap(V2, [4]bool).init(allocator);
+    // defer cache.deinit();
     //
-    // var row_tot: usize = 0;
-    // for (rows.keys()) |r| {
-    //     var in: bool = false;
-    //     for (lines[r]) |c| {
-    //         if ((c == region.crop) != in) {
-    //             row_tot += 1;
-    //             in = !in;
-    //         }
-    //     }
-    //     if (in) {
-    //         row_tot += 1;
-    //     }
-    //     std.debug.print("after: {d}\n", .{row_tot});
+    // const grid = try zutils.Grid(u8).init2DSlice(allocator, lines);
+    // defer grid.deinit();
+    // const list = try allocator.alloc(zutils.V2(usize), region.border.count());
+    // defer allocator.free(list);
+    // for (region.border.keys(), 0..) |v, i| {
+    //     list[i] = v.asType(usize);
     // }
     //
-    // var col_tot: usize = 0;
-    // for (cols.keys()) |j| {
-    //     var in: bool = false;
-    //     var i: usize = 0;
-    //     while (i < lines.len) : (i += 1) {
-    //         const c = lines[i][j];
-    //         if ((c == region.crop) != in) {
-    //             col_tot += 1;
-    //             in = !in;
+    // grid.printHl(list);
+    //
+    // for (region.border.keys()) |start| {
+    //     std.debug.print("start: {}\n", .{start});
+    //     for (start.neighbors(), 0..) |n, dir_idx| {
+    //         if (getCrop(lines, n) == region.crop) {
+    //             // looking in
+    //             continue;
+    //         }
+    //         // this is a direction out of the region
+    //         const start_cache = try cache.getOrPutValue(start, .{ false, false, false, false });
+    //         if (start_cache.value_ptr[dir_idx]) {
+    //             std.debug.print("  cached {}\n", .{n.sub(start)});
+    //             continue;
+    //         }
+    //
+    //         const dir = n.sub(start);
+    //         std.debug.print("  dir: {}\n", .{dir});
+    //
+    //         start_cache.value_ptr[dir_idx] = true;
+    //         sides += 1;
+    //
+    //         for (region.border.keys()) |other| {
+    //             if (other.equal(start)) {
+    //                 continue;
+    //             }
+    //
+    //             const check = if (dir.x != 0) other.x == start.x else other.y == start.y;
+    //             if (check and borderPathExists(lines, region, start, other)) {
+    //                 std.debug.print("  same side as: {}\n", .{other});
+    //                 const o_cache = try cache.getOrPutValue(other, .{ false, false, false, false });
+    //                 o_cache.value_ptr[dir_idx] = true;
+    //             }
     //         }
     //     }
-    //     if (in) {
-    //         col_tot += 1;
-    //     }
     // }
-    // std.debug.print("hemmmm: {d}\n", .{col_tot + row_tot});
-    // printPlot(lines, region);
-    // return col_tot + row_tot;
+    //
+    // return sides;
 }
 
 fn p1Total(regions: *const std.ArrayList(Region)) usize {
@@ -213,28 +278,26 @@ fn p1Total(regions: *const std.ArrayList(Region)) usize {
 
 fn p2Total(
     allocator: std.mem.Allocator,
-    lines: []const []const u8,
+    lines: Lines,
     regions: *const std.ArrayList(Region),
 ) !usize {
     var tot: usize = 0;
-    for (regions.items) |*r| {
-        tot += (try findRegionSides(allocator, r, lines)) * r.area;
-        // TODO
+    for (regions.items[2..]) |*r| {
+        const sides = try findRegionSides(allocator, r, lines);
+        std.debug.print("{c}: {d}\n", .{ r.crop, sides });
+        tot += sides * r.area;
         break;
     }
     return tot;
 }
 
-fn problem(allocator: std.mem.Allocator, lines: []const []const u8) ![2]usize {
+fn problem(allocator: std.mem.Allocator, lines: Lines) ![2]usize {
     const regions = try parseRegions(allocator, lines);
     defer {
         for (regions.items) |*r| {
             r.deinit();
         }
         regions.deinit();
-    }
-    for (regions.items) |r| {
-        printRegion(lines, &r);
     }
 
     return .{
