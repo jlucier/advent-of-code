762d33fe6105018c94dc656ae98b63aebab9c9ef
jlucier <jordan@jordanlucier.com>
2025-12-05 18:19:27 -0500

    2025/4

--- 2025/4.zig
+++ 2025/4.zig
@@ -1,25 +1,50 @@
 const std = @import("std");
 const zutils = @import("zutils");
 
-fn parts(gpa: std.mem.Allocator, input: []const u8) !usize {
-    const lines = try zutils.str.splitScalar(gpa, input, '\n');
-    defer gpa.free(lines);
-    const g = try zutils.Grid(u8).init2DSlice(gpa, lines);
-    defer g.deinit();
-
-    var p1: usize = 0;
+fn findRolls(gpa: std.mem.Allocator, g: *const zutils.Grid(u8)) ![]zutils.vec.V2u {
     var giter = g.iterator();
+    var remove = std.array_list.Managed(zutils.vec.V2u).init(gpa);
 
     while (giter.next()) |loc| {
+        if (g.atV(loc) != '@') continue;
+
         var occupied: u8 = 0;
         var nbiter = g.neighbors(loc, .all);
         while (nbiter.next()) |nb| {
             occupied += @intFromBool(g.atV(nb) == '@');
         }
-        p1 += @intFromBool(occupied < 4);
+        if (occupied < 4) {
+            try remove.append(loc);
+        }
     }
 
-    return p1;
+    return remove.toOwnedSlice();
+}
+
+fn parts(gpa: std.mem.Allocator, input: []const u8) ![2]usize {
+    const lines = try zutils.str.splitScalar(gpa, input, '\n');
+    defer gpa.free(lines);
+    var g = try zutils.Grid(u8).init2DSlice(gpa, lines);
+    defer g.deinit();
+
+    const p1 = try findRolls(gpa, &g);
+    defer gpa.free(p1);
+    var p2: usize = 0;
+
+    while (true) {
+        const remove = try findRolls(gpa, &g);
+        defer gpa.free(remove);
+
+        if (remove.len == 0) break;
+
+        p2 += remove.len;
+
+        for (remove) |loc| {
+            g.atPtrV(loc).* = '.';
+        }
+    }
+
+    return .{ p1.len, p2 };
 }
 
 test "example" {
@@ -37,7 +62,12 @@ test "example" {
     ;
 
     const res = try parts(std.testing.allocator, input);
-    try std.testing.expectEqual(13, res);
+    try std.testing.expectEqual(13, res[0]);
+    try std.testing.expectEqual(43, res[1]);
 }
 
-pub fn main() !void {}
+pub fn main() !void {
+    const input = try zutils.fs.readFile(std.heap.page_allocator, "~/sync/dev/aoc_inputs/2025/4.txt");
+    const res = try parts(std.heap.page_allocator, input);
+    std.debug.print("p1: {d}\np2: {d}\n", .{ res[0], res[1] });
+}
