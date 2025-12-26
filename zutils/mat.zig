d2d9af04d9c565126216b66b5924f91def5a16f3
jlucier <jordan@jordanlucier.com>
2025-12-26 01:14:48 -0500

    Checkpoint with dynamic

--- zutils/mat.zig
+++ zutils/mat.zig
@@ -1,62 +1,96 @@
 const std = @import("std");
 
-pub fn Mat2i(comptime M: usize, comptime N: usize) type {
-    return Mat2(isize, M, N);
-}
+pub const Mat2i = Mat2(isize);
+pub const Mat2f = Mat2(f64);
 
-pub fn Mat2f(comptime M: usize, comptime N: usize) type {
-    return Mat2(f64, M, N);
-}
-
-pub fn Mat2(comptime T: type, comptime M: usize, comptime N: usize) type {
+pub fn Mat2(comptime T: type) type {
     return struct {
-        data: [M * N]T = undefined,
+        data: []T = undefined,
+        M: usize = 0,
+        N: usize = 0,
+        gpa: std.mem.Allocator,
 
         const Self = @This();
 
+        pub fn init(gpa: std.mem.Allocator, M: usize, N: usize) !Self {
+            return .{
+                .data = try gpa.alloc(T, M * N),
+                .M = M,
+                .N = N,
+                .gpa = gpa,
+            };
+        }
+
+        pub fn deinit(self: *const Self) void {
+            self.gpa.free(self.data);
+        }
+
+        pub fn fromSlice(gpa: std.mem.Allocator, M: usize, sl: []const T) !Self {
+            std.debug.assert(sl.len % M == 0);
+            const ret = try Self.init(gpa, M, @divTrunc(sl.len, M));
+            for (sl, 0..) |v, i| ret.data[i] = v;
+            return ret;
+        }
+
+        pub fn zeros(gpa: std.mem.Allocator, M: usize, N: usize) !Self {
+            var res = try Self.init(gpa, M, N);
+            for (&res.data) |*x| x.* = 0;
+            return res;
+        }
+
+        pub fn eye(gpa: std.mem.Allocator, M: usize, N: usize) !Self {
+            var res = try Self.zeros(gpa, M, N);
+            for (0..M) |i| {
+                res.atPtr(i, i).* = 1;
+            }
+            return res;
+        }
+
         pub fn at(self: *const Self, r: usize, c: usize) T {
-            return self.data[r * N + c];
+            return self.data[r * self.N + c];
         }
 
         pub fn atPtr(self: *Self, r: usize, c: usize) *T {
-            return &self.data[r * N + c];
+            return &self.data[r * self.N + c];
         }
 
         pub fn row(self: *Self, i: usize) []T {
-            std.debug.assert(i < M);
-            const st = i * N;
-            return self.data[st .. st + N];
+            std.debug.assert(i < self.M);
+            const st = i * self.N;
+            return self.data[st .. st + self.N];
         }
 
         fn swapRow(self: *Self, a: usize, b: usize) void {
-            std.debug.assert(a >= 0 and a < M and b >= 0 and b < M);
-            for (0..N) |c| {
+            std.debug.assert(a >= 0 and a < self.M and b >= 0 and b < self.M);
+            for (0..self.N) |c| {
                 const tmp = self.at(a, c);
                 self.atPtr(a, c).* = self.at(b, c);
                 self.atPtr(b, c).* = tmp;
             }
         }
 
-        pub fn U(self: *const Self) Self {
-            var new = Self{};
-            std.mem.copyForwards(T, &new.data, &self.data);
-
+        /// Find a pivot in an assumed upper triangular (or close) matrix starting from
+        /// position startPos
+        fn findPivot(self: *const Self, startRow: usize) ?[2]usize {
             var pCol: usize = 0;
-            var finishedRows: usize = 0;
-            while (pCol < N) : (pCol += 1) {
-                var pRow: usize = 0;
-                var hasPivot = false;
-
-                // determine which row is a candidate
-                for (finishedRows..M) |ri| {
-                    if (new.at(ri, pCol) != 0) {
-                        pRow = ri;
-                        hasPivot = true;
-                        break;
+            while (pCol < self.N) : (pCol += 1) {
+                for (startRow..self.M) |ri| {
+                    if (self.at(ri, pCol) != 0) {
+                        return .{ ri, pCol };
                     }
                 }
+            }
+            return null;
+        }
 
-                if (!hasPivot) continue;
+        pub fn U(self: *const Self) !Self {
+            var new = try Self.init(self.gpa, self.M, self.N);
+            std.mem.copyForwards(T, new.data, self.data);
+
+            var finishedRows: usize = 0;
+            while (new.findPivot(finishedRows)) |ret| {
+                var pRow = ret[0];
+                const pCol = ret[1];
 
                 // ensure pivot is in correct row
                 if (pRow != pCol) {
@@ -67,13 +101,13 @@ pub fn Mat2(comptime T: type, comptime M: usize, comptime N: usize) type {
                 // reduce down
                 const pivot = new.at(pRow, pCol);
                 std.debug.assert(pivot != 0);
-                for (pRow + 1..M) |ri| {
+                for (pRow + 1..self.M) |ri| {
                     const factor = switch (@typeInfo(T)) {
                         .int => @divTrunc(new.at(ri, pCol), pivot),
                         else => new.at(ri, pCol) / pivot,
                     };
 
-                    for (0..N) |ci| {
+                    for (0..self.N) |ci| {
                         new.atPtr(ri, ci).* = new.at(ri, ci) - new.at(pRow, ci) * factor;
                     }
                 }
@@ -83,24 +117,12 @@ pub fn Mat2(comptime T: type, comptime M: usize, comptime N: usize) type {
             return new;
         }
 
-        pub fn rref(self: *const Self) Self {
-            var new = self.U();
-            var pCol: usize = 0;
+        pub fn rref(self: *const Self) !Self {
+            var new = try self.U();
             var finishedRows: usize = 0;
-            while (pCol < N) : (pCol += 1) {
-                var pRow: usize = 0;
-                var hasPivot = false;
-
-                // determine which row is a candidate
-                for (finishedRows..M) |ri| {
-                    if (new.at(ri, pCol) != 0) {
-                        pRow = ri;
-                        hasPivot = true;
-                        break;
-                    }
-                }
-
-                if (!hasPivot) continue;
+            while (new.findPivot(finishedRows)) |ret| {
+                var pRow = ret[0];
+                const pCol = ret[1];
 
                 // normalize pivot
                 const pivot = new.at(pRow, pCol);
@@ -116,7 +138,7 @@ pub fn Mat2(comptime T: type, comptime M: usize, comptime N: usize) type {
                     const ri = pRow - 1;
                     // pivot is now 1, factor is just the value at that location
                     const factor = new.at(ri, pCol);
-                    for (0..N) |ci| {
+                    for (0..self.N) |ci| {
                         new.atPtr(ri, ci).* = new.at(ri, ci) - new.at(pRow, ci) * factor;
                     }
                 }
@@ -126,17 +148,45 @@ pub fn Mat2(comptime T: type, comptime M: usize, comptime N: usize) type {
             return new;
         }
 
-        pub fn print(self: *const Self) void {
-            std.debug.print("<Mat2({s}) {d}x{d}\n", .{ @typeName(T), M, N });
+        /// Solve for the simplest solution to the system if one exists
+        pub fn solve(self: *const Self) !?[]T {
+            const R = try self.rref();
+            defer R.deinit();
+            // check for no solutions
+            outer: for (0..self.M) |r| {
+                for (0..self.N - 1) |c| {
+                    if (R.at(r, c) != 0) continue :outer;
+                }
+                // row is all zero, check for contradiction
+                if (R.at(r, self.N - 1) != 0) return null;
+            }
 
-            for (0..M) |r| {
-                std.debug.print("  {any}\n", .{self.data[r * N .. (r + 1) * N]});
+            var soln = try self.gpa.alloc(T, self.N - 1);
+            for (soln) |*x| x.* = 0;
+
+            var pRow: usize = 0;
+            while (R.findPivot(pRow)) |ret| {
+                pRow = ret[0];
+                const pCol = ret[1];
+
+                soln[pCol] = R.at(pRow, self.N - 1);
+                pRow += 1;
+            }
+
+            return soln;
+        }
+
+        pub fn print(self: *const Self) void {
+            std.debug.print("<Mat2({s}) {d}x{d}\n", .{ @typeName(T), self.M, self.N });
+
+            for (0..self.M) |r| {
+                std.debug.print("  {any}\n", .{self.data[r * self.N .. (r + 1) * self.N]});
             }
             std.debug.print(">\n", .{});
         }
 
         pub fn eql(self: *const Self, other: *const Self) bool {
-            return std.mem.eql(T, &self.data, &other.data);
+            return std.mem.eql(T, self.data, other.data);
         }
 
         pub fn approxEql(self: *const Self, other: *const Self, atol: T, rtol: T) bool {
@@ -151,67 +201,92 @@ pub fn Mat2(comptime T: type, comptime M: usize, comptime N: usize) type {
     };
 }
 
-test "Mat.U" {
-    const M = Mat2i(3, 4){
-        .data = .{
-            1, 3, 3, 2, //
-            2, 6, 9, 7, //
-            -1, -3, 3, 4, //
-        },
-    };
-
-    const M2 = Mat2i(3, 4){
-        .data = .{
-            1, 2, 3, 5, //
-            2, 4, 8, 12, //
-            3, 6, 7, 13, //
-        },
-    };
-
-    const M3 = Mat2i(3, 3){
-        .data = .{
-            2, 1, 1, //
-            4, -6, 0, //
-            -2, 7, 2, //
-        },
-    };
-
-    try std.testing.expect(M.U().eql(&Mat2i(3, 4){
-        .data = .{
-            1, 3, 3, 2, //
-            0, 0, 3, 3, //
-            0, 0, 0, 0, //
-        },
-    }));
-    try std.testing.expect(M2.U().eql(&Mat2i(3, 4){
-        .data = .{
-            1, 2, 3, 5, //
-            0, 0, 2, 2, //
-            0, 0, 0, 0, //
-        },
-    }));
-    try std.testing.expect(M3.U().eql(&Mat2i(3, 3){
-        .data = .{
-            2, 1, 1, //
-            0, -8, -2, //
-            0, 0, 1, //
-        },
-    }));
-}
-
 test "Mat.rref" {
-    const M = Mat2i(3, 4){
-        .data = .{
-            1, 3, 3, 2, //
-            2, 6, 9, 7, //
-            -1, -3, 3, 4, //
-        },
-    };
-    try std.testing.expect(M.rref().eql(&Mat2i(3, 4){
-        .data = .{
-            1, 3, 0, -1, //
-            0, 0, 1, 1, //
-            0, 0, 0, 0, //
-        },
-    }));
+    const M = try Mat2i.fromSlice(std.testing.allocator, 3, &[_]isize{
+        1, 3, 3, 2, //
+        2, 6, 9, 7, //
+        -1, -3, 3, 4, //
+    });
+    defer M.deinit();
+    const R = try M.rref();
+    defer R.deinit();
+    const exp = try Mat2i.fromSlice(std.testing.allocator, 3, &[_]isize{
+        1, 3, 0, -1, //
+        0, 0, 1, 1, //
+        0, 0, 0, 0, //
+    });
+    defer exp.deinit();
+    try std.testing.expect(R.eql(&exp));
+
+    //     const M2 = Mat2i.init(std.testing.allocator, 3, 3){
+    //         .data = .{
+    //             2, 1, 1, //
+    //             4, -6, 0, //
+    //             -2, 7, 2, //
+    //         },
+    //     };
+    //     try std.testing.expect(M2.rref().eql(&Mat2i.init(std.testing.allocator, 3, 3){
+    //         .data = .{
+    //             1, 0, 0, //
+    //             0, 1, 0, //
+    //             0, 0, 1, //
+    //         },
+    //     }));
+    //
+    //     // same as first with extra col
+    //     const M3 = Mat2i.init(std.testing.allocator, 3, 5){
+    //         .data = .{
+    //             1, 3, 3, 2, 1, //
+    //             2, 6, 9, 7, 5, //
+    //             -1, -3, 3, 4, 5, //
+    //         },
+    //     };
+    //     try std.testing.expect(M3.rref().eql(&Mat2i.init(std.testing.allocator, 3, 5){
+    //         .data = .{
+    //             1, 3, 0, -1, -2, //
+    //             0, 0, 1, 1, 1, //
+    //             0, 0, 0, 0, 0, //
+    //         },
+    //     }));
 }
+
+test "Mat.solve" {
+    const M = try Mat2i.fromSlice(
+        std.testing.allocator,
+        3,
+        &[_]isize{
+            1, 3, 3, 2, 1, //
+            2, 6, 9, 7, 5, //
+            -1, -3, 3, 4, 5, //
+        },
+    );
+    defer M.deinit();
+    const res = (try M.solve()).?;
+    defer std.testing.allocator.free(res);
+    try std.testing.expectEqualSlices(isize, &.{ -2, 0, 1, 0 }, res);
+
+    // const M2 = Mat2i.init(std.testing.allocator, 3, 5){
+    //     .data = .{
+    //         1, 2, 3, 5, 0, //
+    //         2, 4, 8, 12, 6, //
+    //         3, 6, 7, 13, -6, //
+    //     },
+    // };
+    // try std.testing.expectEqualSlices(isize, &.{ -9, 0, 3, 0 }, &M2.solve().?);
+}
+
+// test "eye" {
+//     try std.testing.expect(Mat2i.eye(std.testing.allocator, 2, 2).eql(&Mat2i.init(std.testing.allocator, 2, 2){
+//         .data = .{
+//             1, 0, //
+//             0, 1, //
+//         },
+//     }));
+//     try std.testing.expect(Mat2i.eye(std.testing.allocator, 3, 4).eql(&Mat2i.init(std.testing.allocator, 3, 4){
+//         .data = .{
+//             1, 0, 0, 0, //
+//             0, 1, 0, 0, //
+//             0, 0, 1, 0, //
+//         },
+//     }));
+// }
